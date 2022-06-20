697adcb904a9909bf9d08e55b0a580305f562eb1
Jacket <irostrasz@gmail.com>
2022-06-20 20:00:32 +0100

    Cambios finales en la estructura del archivo, tomar como referencia para futuros archivos.

--- colegio.sql
+++ colegio.sql
@@ -3,6 +3,9 @@
 DROP DATABASE IF EXISTS miprimerabase
 CREATE DATABASE miprimerabase
 USE miprimerabase
+				--========================================
+				--========== CREACIÓN DE TABLAS ==========
+				--========================================
 
 -- Tabla estudiantes, simple con datos falsos. DNI se limita a 9 y gender a 1 letra (M/F)
 -- La ID se genera automáticamente, ya no hay que insertarla manualmente en el INSERT INTO.
@@ -15,7 +18,59 @@ USE miprimerabase
 		student_dni CHAR(9),
 		student_grade VARCHAR(255),
 		student_dateofbirth DATE
-)
+	);
+
+-- Creo la tabla de los profesores. Esta tabla quiero intentar linkearla a otras usando llaves.
+-- Lo mismo que la anterior y las siguientes, ID autoincremental.
+	CREATE TABLE  teachers (
+		teacher_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+		teacher_name VARCHAR(255),
+		teacher_surname VARCHAR(255),
+		teacher_country VARCHAR(255),
+		teacher_gender VARCHAR(1),
+		teacher_dni CHAR(9),
+		teacher_dateofbirth DATE
+	);
+
+-- Última tabla de momento en esta primera tabla de prueba. Los edificios donde se imparten las clases.
+-- Mi idea es poder relacionarlo todo, aunque necesito investigar más sobre el diseño de las bases en si.
+-- Tabla edificios, una vez más, todo inventado.
+	CREATE TABLE facilities (
+		facility_id INT NOT NULL AUTO_INCREMENT,
+		facility_building_name VARCHAR(255),
+		facilitiy_location VARCHAR(255),
+		FOREIGN KEY (facility_id) REFERENCES grade_and_facility(gf_id)
+	);
+
+-- Creo la tabla de asignaturas. En esta tabla voy a probar a poner más de 1 FK.
+	CREATE TABLE grade (
+		grade_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+		grade_name VARCHAR(255),
+		grade_initials VARCHAR (255),
+		grade_description VARCHAR(255)
+	);
+
+-- Como medida de solucionar los problemas que he tenido, voy a crear tablas intermedias para relacionar
+-- a los profesores con las asignaturas, y luego para relacionar asignaturas con edificios.
+	CREATE TABLE teacher_and_grade (
+		tg_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+		tg_teachername VARCHAR(255),
+		tg_teachersurname VARCHAR(255),
+		tg_teachergrade VARCHAR(255)
+	);
+
+-- Segunda tabla de referencias, esta junta a las asignaturas con el edificio en el que se imparten.
+-- En esta parte estoy muy confundido, tengo que enfocar mas esfuerzos en el diseño de las bases y sus relaciones.
+	CREATE TABLE grade_and_facility (
+		gf_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, --esta debería referenciar a facility_id
+		gf_facname VARCHAR(255),
+		gf_facgrade VARCHAR(255)
+	);
+
+				--===================================================
+				--=========== RELLENO LAS TABLAS CON DATOS ==========
+				--===================================================
+
 -- Insertamos datos de estudiantes. Todos falsos y random. No se añade ID para que se genere sola.
 		INSERT INTO students 
 		(student_name, student_surname, student_country, student_gender, student_dni, student_grade, student_dateofbirth) VALUES
@@ -30,111 +85,62 @@ USE miprimerabase
 			('Carlos', 'Santana', 'España', 'M', '89123456C', 'Estudios de policía nacional', '1994-05-05'),
 			('Stella', 'Guillén', 'Argentina', 'F', '91234567S', 'Modelado 3D',  '1985-04-22');
 
--- Creo la tabla de los profesores. Esta tabla quiero intentar linkearla a otras usando llaves.
--- Lo mismo que la anterior y las siguientes, ID autoincremental.
-	CREATE TABLE  teachers (
-		teacher_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-		teacher_name VARCHAR(255),
-		teacher_surname VARCHAR(255),
-		teacher_country VARCHAR(255),
-		teacher_gender VARCHAR(1),
-		teacher_dni CHAR(9),
-		teacher_dateofbirth DATE
-	)
+
 -- Se insertan datos de profesores, falsos también.
 		INSERT INTO teachers (teacher_name, teacher_surname, teacher_country, teacher_gender, teacher_dni, teacher_dateofbirth) VALUES
-		('Vin', 'Diesel', 'España', 'M', '62487562V', '1940-02-02'),
-		('Jason', 'Statham', 'Reino Unido', 'M', '32568475J', '1965-05-05'),
-		('Calia', 'Menethil', 'Lordaeron', 'F', '46978135C', '1984-06-06'),
-		('Scarlett', 'Johansson', 'Finlandia', 'F', '51426352S', '1975-04-04'),
-		('Eva', 'Green', 'Inglaterra', 'F', '73914685E', '1955-08-08'),
-		('Elizabeth', 'DeWitt', 'Columbia', 'F', '36251474E', '1998-09-29'),
-		('Chuck', 'Norris', 'Estados Unidos', 'M', '74143696C', '1950-07-07'),
-		('Jackie', 'Chan', 'China', 'M', '45658525J', '1945-02-02'),
-		('Artiom', 'Metrinsky', 'Rusia', 'M', '74359615A', '1988-08-08');
-
--- Última tabla de momento en esta primera tabla de prueba. Los edificios donde se imparten las clases.
--- Mi idea es poder relacionarlo todo, aunque necesito investigar más sobre el diseño de las bases en si.
--- Tabla edificios, una vez más, todo inventado.
-	CREATE TABLE facilities (
-		facility_id INT NOT NULL AUTO_INCREMENT,
-		facility_building_name VARCHAR(255),
-		facilitiy_location VARCHAR(255),
-		FOREIGN KEY (facility_id) REFERENCES grade_and_facility(gf_id)
-	);
+			('Vin', 'Diesel', 'España', 'M', '62487562V', '1940-02-02'),
+			('Jason', 'Statham', 'Reino Unido', 'M', '32568475J', '1965-05-05'),
+			('Calia', 'Menethil', 'Lordaeron', 'F', '46978135C', '1984-06-06'),
+			('Scarlett', 'Johansson', 'Finlandia', 'F', '51426352S', '1975-04-04'),
+			('Eva', 'Green', 'Inglaterra', 'F', '73914685E', '1955-08-08'),
+			('Elizabeth', 'DeWitt', 'Columbia', 'F', '36251474E', '1998-09-29'),
+			('Chuck', 'Norris', 'Estados Unidos', 'M', '74143696C', '1950-07-07'),
+			('Jackie', 'Chan', 'China', 'M', '45658525J', '1945-02-02'),
+			('Artiom', 'Metrinsky', 'Rusia', 'M', '74359615A', '1988-08-08');
 
 -- Ahora inserto datos de los edificios. En teoría, si todo va bien, la tabla anterior debería referenciar
 -- correctamente a esta en tanto grade_facility --> facility_id.
 		INSERT INTO facilities (facility_building_name, facilitiy_location) VALUES
-		('Edificio Victoria', 'Ala norte'),
-		('Edificio Esfuerzo', 'Ala sur'),
-		('Edificio Dedicación', 'Ala este'),
-		('Edificio Constancia', 'Ala oeste');
-
--- Creo la tabla de asignaturas. En esta tabla voy a probar a poner más de 1 FK.
-	CREATE TABLE grade (
-		grade_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-		grade_name VARCHAR(255),
-		grade_initials VARCHAR (255),
-		grade_description VARCHAR(255)
---		grade_tutor INT NOT NULL,
---		grade_facility INT NOT NULL,
---		FOREIGN KEY (grade_tutor) REFERENCES teachers(teacher_id),
---		FOREIGN KEY (grade_facility) REFERENCES facilities(facility_id)
-	);
+			('Edificio Victoria', 'Ala norte'),
+			('Edificio Esfuerzo', 'Ala sur'),
+			('Edificio Dedicación', 'Ala este'),
+			('Edificio Constancia', 'Ala oeste');
 
 -- Se insertan datos de las asignaturas a tomar. Lo mismo, todo inventado. En este punto, al probar
 -- el código por partes, la referencia a la tabla facilities da error porque todavía no se ha creado
 -- pero es válida. Aquí en teoría los campos grade_tutor y grade_facility se deberían rellenar solor
 -- usando los datos de las tablas que referencian.
 		INSERT INTO grade (grade_name, grade_initials, grade_description) VALUES
-		('Administración de sistemas informáticos en red', 'ASIR', 'Ahondar en las tecnologías de red'),
-		('Estudios de policía nacional', 'ESPOL', 'Aprender a proteger y servir'),
-		('Modelado 3D', 'MODE', 'Aprender a hacer modelos 3D'),
-		('Diseño de aplicaciones multiplataforma', 'DAM', 'El uso de herramientas para desarrollar aplicaciones'),
-		('Dirección de empresas', 'DIEM', 'Cómo llevar una empresa'),
-		('Cuidado social', 'CUSO', 'Ayuda a personas'),
-		('Anestesia y cirugía elemental', 'ANES', 'Aprender a ser anestesista'),
-		('Filosofía', 'FILO', 'Para pensar, señores'),
-		('Ecología', 'ECOL', 'Para cuidar el medioambiente, señores');
+			('Administración de sistemas informáticos en red', 'ASIR', 'Ahondar en las tecnologías de red'),
+			('Estudios de policía nacional', 'ESPOL', 'Aprender a proteger y servir'),
+			('Modelado 3D', 'MODE', 'Aprender a hacer modelos 3D'),
+			('Diseño de aplicaciones multiplataforma', 'DAM', 'El uso de herramientas para desarrollar aplicaciones'),
+			('Dirección de empresas', 'DIEM', 'Cómo llevar una empresa'),
+			('Cuidado social', 'CUSO', 'Ayuda a personas'),
+			('Anestesia y cirugía elemental', 'ANES', 'Aprender a ser anestesista'),
+			('Filosofía', 'FILO', 'Para pensar, señores'),
+			('Ecología', 'ECOL', 'Para cuidar el medioambiente, señores');
 		
--- Como medida de solucionar los problemas que he tenido, voy a crear tablas intermedias para relacionar
--- a los profesores con las asignaturas, y luego para relacionar asignaturas con edificios.
-	CREATE TABLE teacher_and_grade (
-		tg_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-		tg_teachername VARCHAR(255),
-		tg_teachersurname VARCHAR(255),
-		tg_teachergrade VARCHAR(255)
-	);
-
 -- Y ahora reparto asignaturas a los profesores.
 		INSERT INTO teacher_and_grade (tg_teachername, tg_teachersurname, tg_teachergrade) VALUES
-		('Vin', 'Diesel', 'Dirección de empresas'),
-		('Jason', 'Statham', 'Ecología'),
-		('Calia', 'Menethil', 'Cuidado social'),
-		('Scarlett', 'Johansson', 'Diseño de aplicaciones multiplataforma'),
-		('Eva', 'Green', 'Administración de sistemas informáticos en red'),
-		('Elizabeth', 'DeWitt', 'Filosofía'),
-		('Chuck', 'Norris', 'Anestesia y cirugía elemental'),
-		('Jackie', 'Chan', 'Modelado 3D'),
-		('Artiom', 'Metrinsky', 'Estudios de policía nacional');
-
--- Segunda tabla de referencias, esta junta a las asignaturas con el edificio en el que se imparten.
--- En esta parte estoy muy confundido, tengo que enfocar mas esfuerzos en el diseño de las bases y sus relaciones.
-	CREATE TABLE grade_and_facility (
-		gf_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, --esta debería referenciar a facility_id
-		gf_facname VARCHAR(255),
-		gf_facgrade VARCHAR(255)
-	);
+			('Vin', 'Diesel', 'Dirección de empresas'),
+			('Jason', 'Statham', 'Ecología'),
+			('Calia', 'Menethil', 'Cuidado social'),
+			('Scarlett', 'Johansson', 'Diseño de aplicaciones multiplataforma'),
+			('Eva', 'Green', 'Administración de sistemas informáticos en red'),
+			('Elizabeth', 'DeWitt', 'Filosofía'),
+			('Chuck', 'Norris', 'Anestesia y cirugía elemental'),
+			('Jackie', 'Chan', 'Modelado 3D'),
+			('Artiom', 'Metrinsky', 'Estudios de policía nacional');
 
 -- Inserto valores, en esta parte estoy bastante perdido, tengo que encontrar alguien que pueda aclararme dudas.
 		INSERT INTO grade_and_facility (gf_facname, gf_facgrade) VALUES
-		('Edificio Victoria', 'Dirección de empresas'),
-		('Edificio Esfuerzo', 'Ecología'),
-		('Edificio Dedicación', 'Cuidado social'),
-		('Edificio Constancia', 'Diseño de aplicaciones multiplataforma'),
-		('Edificio Victoria', 'Administración de sistemas informáticos en red'),
-		('Edificio Esfuerzo', 'Filosofía'),
-		('Edificio Dedicación', 'Anestesia y cirugía elemental'),
-		('Edificio Constancia', 'Modelado 3D'),
-		('Edificio Victoria', 'Estudios de policía nacional');
+			('Edificio Victoria', 'Dirección de empresas'),
+			('Edificio Esfuerzo', 'Ecología'),
+			('Edificio Dedicación', 'Cuidado social'),
+			('Edificio Constancia', 'Diseño de aplicaciones multiplataforma'),
+			('Edificio Victoria', 'Administración de sistemas informáticos en red'),
+			('Edificio Esfuerzo', 'Filosofía'),
+			('Edificio Dedicación', 'Anestesia y cirugía elemental'),
+			('Edificio Constancia', 'Modelado 3D'),
+			('Edificio Victoria', 'Estudios de policía nacional');
